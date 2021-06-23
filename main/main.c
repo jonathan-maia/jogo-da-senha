cf8fa6b2603b5ef362f334a88877caa39c2e52c2
Jonathan Tauan <jtauan10@gmail.com>
2021-06-22 23:59:27 -0300

    update teste

--- main/main.c
+++ main/main.c
@@ -12,11 +12,11 @@ int main(void)
 
   do{
 
-    //perguntando se irá jogar contra a máquina.
-    printf("Se desejar jogar contra o computador, digite 1. Para jogar contra outra pessoa, digite qualquer outro número :");
+    //perguntando se irï¿½ jogar contra a mï¿½quina.
+    printf("Se desejar jogar contra o computador, digite 1. Para jogar contra outra pessoa, digite qualquer outro nï¿½mero :");
     scanf("%d", &maquina);
 
-    // jogar contra máquina e gerar senha aleatória
+    // jogar contra mï¿½quina e gerar senha aleatï¿½ria
     if(maquina==1){
       srand((unsigned)time(NULL));
       senha = (rand() % v2) +1;
@@ -26,9 +26,9 @@ int main(void)
       printf("Jogador 1 - Digite uma senha entre %d e %d para ser descoberta pelo jogador 2 : ", v1,v2);
       scanf("%d", &senha);
   
-      //verificando se senha está dentro das especificações e pedindo outra senha
+      //verificando se senha estï¿½ dentro das especificaï¿½ï¿½es e pedindo outra senha
       while(senha < v1 || senha > v2){
-        printf("Senha fora do intervalo permitido, digite outra senha : ");
+        printf("Senha fora do intervalo permitido, digite outra senha: ");
         scanf("%d", &senha);
       }
       //limpando terminal
@@ -36,32 +36,32 @@ int main(void)
     }
 
     //Jogador 2
-    printf("Jogador 2 - Você tem %d chances para descobrir a senha ! \n",chances);
+    printf("Jogador 2 - Vocï¿½ tem %d chances para descobrir a senha ! \n",chances);
 
     //palpites e repostas
     for(i = 1; i < chances+1; i++){
-      printf("Dê o %dº palpite : ",i);
+      printf("Dï¿½ o %dï¿½ palpite : ",i);
       scanf("%d", &palpite);
       if(palpite < v1 || palpite > v2){
         printf("O palpite foi fora do intervalo ! (de %d a %d) \n", v1,v2);
         i = i-1;
       }else{
         if(senha == palpite){
-          printf("Você acertou ! A senha era %d ! \n", senha);
-          printf("Deseja jogar novamente ? (Digite 1 para sim ou outro número para sair): ");
+          printf("Vocï¿½ acertou ! A senha era %d ! \n", senha);
+          printf("Deseja jogar novamente ? (Digite 1 para sim ou outro nï¿½mero para sair): ");
           scanf("%d",&continua);
           break;
         }else if(i == 5 ){
-          printf("Você perdeu. Tente novamente depois.\n");
-          printf("Deseja jogar novamente ? (Digite 1 para sim ou outro número para sair)");
+          printf("Vocï¿½ perdeu. Tente novamente depois.\n");
+          printf("Deseja jogar novamente ? (Digite 1 para sim ou outro nï¿½mero para sair)");
           scanf("%d",&continua);
         }else if(senha == palpite-1 || senha == palpite+1){
-          printf("TÁ QUENTE !!! \n");
+          printf("Tï¿½ QUENTE !!! \n");
         }else{
           if(palpite > senha){
-            printf("A senha é menor que %d ! \n",palpite);
+            printf("A senha ï¿½ menor que %d ! \n",palpite);
           }else{
-            printf("A senha é maior que %d! \n",palpite);
+            printf("A senha ï¿½ maior que %d! \n",palpite);
           }
         }
       }
